542fdb371a1f70b0488759e2349879bef60825a9
posiflex555-stack <posiflex555@gmail.com>
2026-07-26 12:56:14 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-// هذا الملف ضروري لتفعيل زر التثبيت كتطبيق (PWA)
-self.addEventListener('install', (event) => {
-  self.skipWaiting();
-});
-
-self.addEventListener('fetch', (event) => {
-  // يمكن تركها فارغة، هي فقط لإيهام المتصفح أن التطبيق يعمل أوفلاين
-});
